c6191978ea83f7f74f52dbb5310c3cfa2cac8fe2
Thomas Lin <t.lin@mail.utoronto.ca>
2021-04-08 20:43:20 -0400

    Refactored code to auto-fetch metadata from ipinfo if not found   - Fetched metadata is cached into etcd for future queries   - Added ipinfo to requirements file

--- server/api/defaults.py
+++ server/api/defaults.py
@@ -6,45 +6,58 @@ from . import rest_api
 
 import etcd3
 import ujson
+import ipaddress
+import ipinfo
 
 # etcd client
 etcd = etcd3.client()
 
-class IpMetadata(Resource):
+# Create ipinfo client/handler
+apiKey = "KEY HERE"
+ipHandler = ipinfo.getHandler(apiKey)
+
+class IPMetadata(Resource):
     def get(self, ip):
-        # TODO: Validate IP address... for now just use it
-
-        metadata = etcd.get(ip)[0]
-        if not metadata:
-            return make_response('{}') # Or return error?
-
-        metadata = ujson.loads(metadata)
-
-        retMsg = "You called GET w/ key %s! Result: %s" % (ip, metadata)
-        return make_response(retMsg)
-
-    def put(self, ip):
-        return self.post(ip)
-
-    def post(self, ip):
-        body = request.get_data()
-        if not body:
-            retMsg = "Missing JSON body with service name and metadata"
-            return make_response(retMsg, status.HTTP_400_BAD_REQUEST)
-
+        # Validate IP address
         try:
-            metadata = ujson.loads(body) # Transform into dict for validation purposes
+            ip = ipaddress.ip_address(ip)
         except Exception as err:
-            retMsg = str(err)
+            return make_response(str(err), status.HTTP_400_BAD_REQUEST)
+
+        if not ip.is_global:
+            retMsg = "ERROR: IP address %s is not global % ip"
             return make_response(retMsg, status.HTTP_400_BAD_REQUEST)
 
-        # TODO: Validate metadata... for now, just store body into etcd
+        # Transform IP back to string for uniformity
+        #   e.g. 192.168.01.10 => 192.168.1.10
+        ip = str(ip)
+
+        ipMeta = etcd.get(ip)[0]
+        if not ipMeta:
+            # Fetch and cache
+            try:
+                ipMeta = ipHandler.getDetails(ip).details
+                self.storeMetadata(ip, ipMeta)
+                ipMeta = str(ipMeta) # Convert for response
+            except Exception as err:
+                retMsg = "ERROR: Unable to get details for %s\n%s" % (ip, err)
+                return make_response(retMsg, status.HTTP_503_SERVICE_UNAVAILABLE)
+        else:
+            # etcd returns byte array, convert to utf-8 string
+            ipMeta = ipMeta.decode('utf-8')
+
+        return make_response(ipMeta)
+
+    def storeMetadata(self, ip, ipMeta):
+        assert type(ipMeta) is dict
+
+        # TODO: Validate metadata? For now, just store body into etcd
 
         # Transform back into string and store into etcd
-        etcd.put(ip, ujson.dumps(metadata))
+        etcd.put(ip, ujson.dumps(ipMeta))
 
         retMsg = "You called PUT w/ key %s!" % ip
         return make_response(retMsg)
 
-rest_api.add_resource(IpMetadata, '/<string:ip>')
+rest_api.add_resource(IPMetadata, '/<string:ip>')
 
