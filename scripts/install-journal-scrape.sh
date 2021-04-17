401c94292dcb2289b0b9356a9743b2ec9d8a92f0
Thomas Lin <t.lin@mail.utoronto.ca>
2021-04-17 18:21:56 -0400

    1) Make ip-metadata-journal-scrape and its installation scripts take a    URL endpoint as input parameter, the base URL of where the IP metadata    service runs. Allows easier customization rather than hard-coding. 2) Modified shebang of ip-metadata-journal-scrape script to run Python3    in unbuffered mode (i.e. always flush), allowing journald to capture    print output immediately

--- scripts/install-journal-scrape.sh
+++ scripts/install-journal-scrape.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+if [[ $# -ne 1 ]]; then
+    echo "ERROR: Expecting only one argument (IP metadata server base URL, e.g. http://localhost:4040)"
+    exit 1
+else
+    IPMETA_BASE_URL=$1
+fi
+
 
 SCRIPT_DIR=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
 LOCAL_BIN_PATH=/usr/local/bin
@@ -6,9 +13,15 @@ LOCAL_BIN_PATH=/usr/local/bin
 # Create systemd service file if it doesn't exist
 SERV_NAME=ip-metadata-journal-scrape
 SYSTEMD_SERVICE=/lib/systemd/system/${SERV_NAME}.service
-if [[ ! -f ${SYSTEMD_SERVICE} ]]; then
-    TMP_SERVICE=`mktemp`
-    cat <<TLINEND >> ${TMP_SERVICE}
+if [[ -f ${SYSTEMD_SERVICE} ]]; then
+    # Previous installation may exist, stop it first
+    echo "Stopping service..."
+    sudo service ${SERV_NAME} stop
+fi
+
+# Always replace systemd service file (in case user changes URL of IP metadata service)
+TMP_SERVICE=`mktemp`
+cat <<TLINEND >> ${TMP_SERVICE}
 [Unit]
 Description=IP Metadata Journal Scraper
 Wants=network-online.target
@@ -17,20 +30,15 @@ After=network-online.target
 [Service]
 Type=simple
 TimeoutStopSec=30
-ExecStart=${LOCAL_BIN_PATH}/${SERV_NAME}
+ExecStart=${LOCAL_BIN_PATH}/${SERV_NAME} ${IPMETA_BASE_URL}
 ExecStop=/bin/kill -TERM \$MAINPID
 
 [Install]
 WantedBy=multi-user.target
 TLINEND
 
-    # Move to final location
-    sudo mv ${TMP_SERVICE} ${SYSTEMD_SERVICE} && sudo chmod og+r ${SYSTEMD_SERVICE}
-else
-    # Previous installation may exist, stop it first
-    echo "Stopping service..."
-    sudo service ${SERV_NAME} stop
-fi
+# Move to final location
+sudo mv ${TMP_SERVICE} ${SYSTEMD_SERVICE} && sudo chmod og+r ${SYSTEMD_SERVICE}
 
 # Move/replace script
 sudo cp -a ${SCRIPT_DIR}/${SERV_NAME} ${LOCAL_BIN_PATH}
